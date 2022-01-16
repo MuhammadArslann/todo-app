ccf75563fb107abc373ef662218df438d5ee737e
MuhammadArslann <ar143150@gmail.com>
2022-01-16 23:09:38 +0500

    todo-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations:[
+    trigger("moveInLeft",[
+       transition("void=> *",[style({transform:"translateX(300px)"}),
+         animate(200,keyframes([
+          style({transform:"translateX(300px)"}),
+          style({transform:"translateX(0)"})
+  
+           ]))]),
+ transition("*=>void",[style({transform:"translateX(0px)"}),
+         animate(200,keyframes([
+          style({transform:"translateX(0px)"}),
+          style({transform:"translateX(300px)"})
+  
+           ]))])    
+      
+     ])
+ ]
 })
 export class AppComponent {
+  @ViewChild('todoForm') todoForms!: NgForm;
   title = 'todo-app';
+  todo = '';
+  todoArray: string[] = [];
+
+  addTodo(value: any) {
+    if(value !== null){
+    this.todoArray.push(value);
+    }
+    this.todoForms.reset();
+    console.log(value);
+  }
+  deleteItem(value: any){
+    for(let i=0; i <= this.todoArray.length; i++){
+      if(value == this.todoArray[i]){
+        this.todoArray.splice(i,1);
+      }
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
+import { animation } from '@angular/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
